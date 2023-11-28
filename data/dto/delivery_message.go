7a7f07829c1e8e78221e6b574c409b41933df50f
tdatIT <tdat.it2k2@gmail.com>
2023-11-28 16:52:48 +0700

    #add: order statics and publish message

--- data/dto/delivery_message.go
+++ data/dto/delivery_message.go
@@ -4,4 +4,5 @@ type DeliveryAccountMessage struct {
 	EmailRecipient string `json:"email_recipient" validator:"email"`
 	Email          string `json:"email"`
 	Password       string `json:"password"`
+	Token          string `json:"token"`
 }
