cd7e838a2fd9a58999d28f2d409fb8826bd3e349
tdatIT <tdat.it2k2@gmail.com>
2023-12-18 06:40:09 +0700

    #add: log

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"email-service/config"
 	"email-service/service"
 	message "email-service/worker"
+	"encoding/json"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
+	"net/http"
 	"sync"
 )
 
@@ -48,6 +51,35 @@ func main() {
 	wg.Add(1)
 	go paymentWorker.ListenMessageQueue(&wg)
 
-	wg.Wait()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		http.HandleFunc("/", wellcomeHandler)
+		http.Handle("/metrics", promhttp.Handler())
 
+		log.Printf("Init email service http://localhost:5015")
+		err := http.ListenAndServe(":5015", nil)
+		if err != nil {
+			log.Fatalf("HTTP server failed: %v", err)
+		}
+
+	}()
+
+	wg.Wait()
+}
+
+func wellcomeHandler(w http.ResponseWriter, r *http.Request) {
+	s := struct {
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Message: "Email service was developed by TienDat",
+		Version: "v0.0.1",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	err := json.NewEncoder(w).Encode(s)
+	if err != nil {
+		return
+	}
 }
